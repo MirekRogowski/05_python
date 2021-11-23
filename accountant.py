b5e2086cc7b0dde1aecf300447b1d8ebe20e1b5b
MirekRogowski <93225975+MirekRogowski@users.noreply.github.com>
2021-11-23 22:34:04 +0100

    Update accountant.py
    conditions

--- accountant.py
+++ accountant.py
@@ -7,20 +7,28 @@ availabe_actions =["saldo", "sprzedaz","zakup","konto","magazyn","przeglad"]
 
 while True:
     print(f"Dozwolone akcje: {availabe_actions}")
-    action = ("Podaj rodzaj akcji: ")
-    if sys.argv[1] == "saldo":
+    if len(sys.argv) > 1 and sys.argv[1]=="saldo":
         print("Dopisz saldo")
-    elif sys.argv[1] == "sprzedaz":
-        print("Dopisz sprzedaz")    
-    elif sys.argv[1] == "zakup":
-        print("Dopisz zakup") 
-    elif sys.argv[1] == "konto":
-        print("Dopisz konto") 
-    elif sys.argv[1] == "magazyn":
-        print("Dopisz magazyn") 
-    elif sys.argv[1] == "przeglad":
-        print("Dopisz przeglad") 
+        exit()
+    elif len(sys.argv) > 1 and sys.argv[1]== "sprzedaz":
+         print("Dopisz sprzedaz") 
+         exit()   
+    elif len(sys.argv) > 1 and sys.argv[1]== "zakup":
+         print("Dopisz zakup") 
+         exit()
+    elif len(sys.argv) > 1 and sys.argv[1]== "konto":
+         print("Dopisz konto") 
+         exit()
+    elif len(sys.argv) > 1 and sys.argv[1]== "magazyn":
+         print("Dopisz magazyn") 
+         exit()
+    elif len(sys.argv) > 1 and sys.argv[1]== "przeglad":
+         print("Dopisz przeglad") 
+         exit()
     else:
-        if sys.argv[1] == True:
-            action = input("Podaj rodzaj akcji: ")
+        print("No arguments")
+        exit()
+
+   
+  
 
